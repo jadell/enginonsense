b6870d3d1f12db01032d3e42af6c340ab317dccd
Josh Adell <josh.adell@gmail.com>
2015-02-24 19:40:01 -0500

    Add generated title to banner

---
+++ fonts.php
@@ -0,0 +1,5 @@
+<?php
+return [
+	['xolonium/Xolonium-Regular.otf', 18],
+	['karla-bold-stencil/Karla_BoldStencil.otf', 18],
+];
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 use Imagine\Gd\Imagine,
+	Imagine\Gd\Font,
 	Imagine\Image\Point,
 	Imagine\Image\Box;
 
@@ -8,21 +9,38 @@ require __DIR__.'/vendor/autoload.php';
 $words = require __DIR__.'/words.php';
 shuffle($words['adjectives']);
 shuffle($words['nouns']);
-$title = ucwords(sprintf('JAD %s %s, Interplanetary',
-	next($words['adjectives']),
-	next($words['nouns'])
-));
-echo $title.PHP_EOL;
+$title = sprintf('JAD %s %s',
+	ucwords(next($words['adjectives'])),
+	ucwords(next($words['nouns']))
+);
 
+// Base logo
 $imagine = new Imagine();
 $base = $imagine->open('base.png');
 $base = $base->resize($base->getSize()->scale(.5));
 $baseHeight = $base->getSize()->getHeight();
 $baseWidth = $base->getSize()->getWidth();
 
+// Title color
 $upperLeft = new Point(0,0);
 $backgroundColor = $base->getColorAt($upperLeft);
+$titleColor = $backgroundColor->lighten(100);
 
-$imagine->create(new Box($baseWidth * 5, $baseHeight), $backgroundColor)
-	->paste($base, $upperLeft)
-	->save('banner.png');
+// Title font
+$fonts = require __DIR__.'/fonts.php';
+shuffle($fonts);
+$titleFont = next($fonts);
+$titleFont = new Font(__DIR__.'/fonts/'.$titleFont[0], $titleFont[1], $titleColor);
+$titleBox = $titleFont->box($title);
+echo $titleFont->getFile().PHP_EOL;
+
+$titleMarginHorizontal = $baseWidth * .125;
+$titleMarginVertical = ($baseHeight / 2) - ($titleBox->getHeight() / 2);
+$titlePosition = new Point($baseWidth + $titleMarginHorizontal, $titleMarginVertical);
+
+// Create banner and save
+$bannerWidth = $baseWidth + ($titleMarginHorizontal * 2) + $titleBox->getWidth();
+$banner = $imagine->create(new Box($bannerWidth, $baseHeight), $backgroundColor);
+$banner->paste($base, $upperLeft);
+$banner->draw()->text($title, $titleFont, $titlePosition);
+$banner->save('banner.png');
